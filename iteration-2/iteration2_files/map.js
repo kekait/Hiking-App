71bd87798904342274beba643a1e1493c301fa81
Shane Kelly <spk86@humboldt.edu>
2016-11-14 19:24:11 -0800

    added weather conditions to windows

--- iteration-2/iteration2_files/map.js
+++ iteration-2/iteration2_files/map.js
@@ -47,6 +47,13 @@ function load() {
 		strokeOpacity: 1.0,
 		strokeWeight: 2
 	});
+	var brackishPondLine = new google.maps.Polyline({
+		path: brackishPond,
+		geodesic: true,
+		strokeColor: '#FF0000',
+		strokeOpacity: 1.0,
+		strokeWeight: 2
+	});
 
 
 	//sets polyline on map and calculates distance
@@ -70,6 +77,11 @@ function load() {
 	redwoodParkDistance = google.maps.geometry.spherical.computeLength(redwoodParkDistance);
 	redwoodParkDistance = redwoodParkDistance * 0.000621371;
 
+	brackishPondLine.setMap(map);
+	var brackishPondDistance = brackishPondLine.getPath();
+	brackishPondDistance = google.maps.geometry.spherical.computeLength(brackishPondDistance);
+	brackishPondDistance = brackishPondDistance * 0.000621371;
+
 
 	//creates marker on map for each trail
 	var kloppMarker = new google.maps.Marker({
@@ -92,24 +104,49 @@ function load() {
 		map: map,
 		title: 'Redwood Park'
 	});
+	var brackishPondMarker = new google.maps.Marker({
+	position: brackishPondStart,
+	map: map,
+	title: 'Brackish Pond'
+});
 
-	
+
 	//Find the sunrise/sunset based on lat/long of trail start
 	var lmc_ar = sunrise_set(lostManStart.lat,lostManStart.lng);
 	var kl_ar = sunrise_set(kloppStart.lat,kloppStart.lng);
 	var ht_ar = sunrise_set(hikshariStart.lat,hikshariStart.lng);
 	var rp_ar = sunrise_set(redwoodParkStart.lat,redwoodParkStart.lng);
-	
+	var bp_ar = sunrise_set(brackishPondStart.lat,brackishPond.lng);
+
+
+        // Creates variables for weather conditions and is currently pulling from only the Arcata API on wunderground.com
+        // Will need to create different variables for areas other than arcata and pull the .json file from wunderground.com for each area... couldn't do it based on geolocation
+        // If someoen can figure out how to do it via geolocation that would be awesome
+        var weather = new XMLHttpRequest();
+        weather.open("GET", "http://api.wunderground.com/api/e2d25049016cd0f7/conditions/q/CA/Arcata.json", false);
+        weather.send(null);
+        var r = JSON.parse(weather.response);
+        var weather = r.current_observation.display_location.full;
+        var temp = r.current_observation.temperature_string;
+        var wind = r.current_observation.wind_string;    
+
 	//String information for trail infowindow
 	var kloppContString = 	'<h2>Klopp Lake</h2>'+ '<p>Trail length: ' + kloppDistance.toFixed(2) + 'mi</p>'
-						+ '<p>Sunrise: ' + kl_ar[0] + '</p>' + '<p>Sunset: ' + kl_ar[1] + '</p>';
+						+ '<p>Sunrise: ' + kl_ar[0] + '</p>' + '<p>Sunset: ' + kl_ar[1] + '</p>'
+	                                        + '<p>Temperature: ' + temp + '</p>' + '<p>Wind Speed: ' + wind + '</p>';
 	var lostManContString =  '<h2>Lost Man Trail</h2>'+ '<p>Trail length: ' + lostManDistance.toFixed(2) + 'mi</p>'
-						+ '<p>Sunrise: ' + lmc_ar[0] + '</p>' + '<p>Sunset: ' + lmc_ar[1] + '</p>';
+						+ '<p>Sunrise: ' + lmc_ar[0] + '</p>' + '<p>Sunset: ' + lmc_ar[1] + '</p>'
+	                                        + '<p>Temperature: ' + temp + '</p>' + '<p>Wind Speed: ' + wind + '</p>';
 	var hikshariContString = '<h2>Hikshari Trail</h2>'+ '<p>Trail length: ' + hikshariDistance.toFixed(2) + 'mi</p>'
-						+ '<p>Sunrise: ' + ht_ar[0] + '</p>' + '<p>Sunset: ' + ht_ar[1] + '</p>';
+						+ '<p>Sunrise: ' + ht_ar[0] + '</p>' + '<p>Sunset: ' + ht_ar[1] + '</p>'
+	                                        + '<p>Temperature: ' + temp + '</p>' + '<p>Wind Speed: ' + wind + '</p>';
 	var redwoodParkContString = '<h2>Redwood Park</h2>' + '<p>Trail length: ' + redwoodParkDistance.toFixed(2) + 'mi</p>'
-						+ '<p>Sunrise: ' + rp_ar[0] + '</p>' + '<p>Sunset: ' + rp_ar[1] + '</p>';
-	
+						+ '<p>Sunrise: ' + rp_ar[0] + '</p>' + '<p>Sunset: ' + rp_ar[1] + '</p>'
+	                                        + '<p>Temperature: ' + temp + '</p>' + '<p>Wind Speed: ' + wind + '</p>';
+	var brackishPondContString = '<h2>Brackish Pond</h2>' + '<p>Trail length: ' + brackishPondDistance.toFixed(2) + 'mi</p>'
+						+ '<p>Sunrise: ' + bp_ar[0] + '</p>' + '<p>Sunset: ' + bp_ar[1] + '</p>' 
+	                                        + '<p>Temperature: ' + temp + '</p>' + '<p>Wind Speed: ' + wind + '</p>';
+
 	//creates infowindow when marker is clicked for each trail
 	var kloppInfo = new google.maps.InfoWindow({
 		content: kloppContString
@@ -122,6 +159,9 @@ function load() {
 	});
 		var redwoodParkInfo = new google.maps.InfoWindow({
 		content: redwoodParkContString
+	});
+		var brackishPondInfo = new google.maps.InfoWindow({
+		content: brackishPondContString
 	});
 
 
@@ -138,6 +178,9 @@ function load() {
 	});
 	redwoodParkMarker.addListener('click', function(){
 		redwoodParkInfo.open(map, redwoodParkMarker);
+	});
+	brackishPondMarker.addListener('click', function(){
+		brackishPondInfo.open(map, brackishPondMarker);
 	});
 
 
@@ -158,16 +201,11 @@ function load() {
 	tester.addEventListener('click',function(){
 		map.setCenter(redwoodParkStart);});
 
-	var infoWindow = new google.maps.InfoWindow;
-	// Change this depending on the name of your PHP file
+	var tester = document.getElementById("brackishPond");
+	tester.addEventListener('click',function(){
+		map.setCenter(brackishPondStart);});
 
-        // sets weather layer onto map
-        var weatherLayer = new google.maps.weather.WeatherLayer({
-	    temperatureUnits: google.maps.weather.TemperatureUnit.FAHRENHEIT
-	});
-        weatherLayer.setMap(map);
-     
-        var cloudLayer = new google.maps.weather.CloudLayer();
-        cloudLayer.setMap(map);
+	var infoWindow = new google.maps.InfoWindow;
+
 
 };
