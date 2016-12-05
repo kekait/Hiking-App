3ab533b80719bd3f5d1b04fa3a7f8abba50e27d1
kekait <ktt107@humboldt.edu>
2016-12-05 00:02:50 -0800

    Update trailNotes.js

--- iteration-3/iteration-3-files/trailNotes.js
+++ iteration-3/iteration-3-files/trailNotes.js
@@ -1,11 +1,12 @@
-
+    // Used to add an infowindow to all the markers queried from the database.
     function bindInfoWindow(marker, map, infoWindow, html) {
       google.maps.event.addListener(marker, 'click', function() {
         infoWindow.setContent(html);
         infoWindow.open(map, marker);
       });
     }
-
+    /* Used to retrieve trail descriptions from user input and save them as well as
+      the marker location in the database */
     function saveData() {
       var name = escape(document.getElementById("name").value);
       var description = escape(document.getElementById("description").value);
@@ -20,6 +21,7 @@
         }
       });
     }
+    // fucntion when called performs an ajax request
     function downloadUrl1(url, callback) {
       var request = window.ActiveXObject ?
           new ActiveXObject('Microsoft.XMLHTTP') :
@@ -35,6 +37,7 @@
       request.open('GET', url, true);
       request.send(null);
     }
+
     function downloadUrl(url, callback) {
       var request = window.ActiveXObject ?
           new ActiveXObject('Microsoft.XMLHTTP') :
